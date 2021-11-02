8676d8b7810e4cf36ac28c2fc511731ffd4cefe8
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-01 21:40:00 -0500

    set up local host

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3007;
+
 
 const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`Get ready to take notes on ${PORT}`);
+});
