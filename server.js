af7aa977e03f60ed6c16d258a98ec108cc0a420e
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-01 21:55:03 -0500

    get route for landing page

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const PORT = process.env.PORT || 3007;
 
 const app = express();
 
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 app.listen(PORT, () => {
     console.log(`Get ready to take notes on ${PORT}`);
 });
