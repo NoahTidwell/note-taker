66a10560928c43f643ef5e15af5d070ab0b0c1ff
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-02 18:58:30 -0500

    read db.json file and set up post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT || 3007;
 
 
@@ -11,14 +12,27 @@ app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        console.log(data);
+    });
+
+});
+
+app.post('/notes', (req, res) => {
+    
 });
 
 
 
 app.listen(PORT, () => {
-    console.log(`Get ready to take notes on ${PORT}`);
+    console.log(`Get ready to take notes on Port ${PORT}`);
 });
