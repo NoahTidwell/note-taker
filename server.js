6570dcd0754dacc3bfa0951e1ed5d742dc60557b
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-03 11:32:09 -0500

    refactor and test POST route in insomnia core

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const db = require('./db/db.json');
+const uuidv4 = require('uuid');
 const PORT = process.env.PORT || 3007;
 
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/', (req, res) => {
+// HTML Notes Page GET Route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+
+});
+
+// POST Route for New Note
+app.post('/notes', (req, res) => {
+    console.log(req.body);
+    res.status(201).send(`Created Note.`);
+});
+
+// HTML Landing Page GET Route
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
     
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err)
-            return;
-        }
-        console.log(data);
-    });
 
-});
-
-app.post('/notes', (req, res) => {
-    
-});
 
 
 
