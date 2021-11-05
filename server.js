926196078ca4f93f705d3d1ec1d021c15d7664aa
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-05 01:37:18 -0500

    fix POST and GET orientation to fix problem

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {
     v1: uuidv1,
     v4: uuidv4,
   } = require('uuid');
-const db = require('./db/db.json');
+let db = require('./db/db.json');
 const PORT = process.env.PORT || 3007;
 
 // Create Server
@@ -16,36 +16,39 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 
-// HTML Notes Page GET Route
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
 
-});
 
 app.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
 // POST Route for New Note
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
+    if (req.body) {
     const newNote = req.body;
 
     // create unique ID for each note
     newNote.id = uuidv4();
     // add newNote to db.json file
     db.push(newNote);
+    // Console new note
     console.log(newNote);
 
-    
     fs.writeFileSync(path.join(__dirname + '/db/db.json'), JSON.stringify(db,null,4));
     // send file to display Notes HTML page
     res.sendFile(path.join(__dirname + '/public/notes.html'));
+    }
 });
 
 // BONUS: DELETE Route for Created Notes
 //app.delete('/api/notes/:id', (req, res) => {
 //});
 
+// HTML Notes Page GET Route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+
+});
 
 // HTML Landing Page GET Route
 app.get('*', (req, res) => {
